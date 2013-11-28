e3ecfc9248adebcd80154c0ee88aa2a5fb84aa6e
ChepKun <ProgrammerKun@gmail.com>
2013-11-28 20:50:45 +0300

    first commit, add configs, overlay, updates in Settings and Vold

---
+++ config/common_full.mk
@@ -0,0 +1,24 @@
+# Inherit common CM stuff
+$(call inherit-product, vendor/yamato/config/common.mk)
+
+# Bring in all audio files
+include frameworks/base/data/sounds/NewAudio.mk
+
+# Extra Ringtones
+include frameworks/base/data/sounds/AudioPackageNewWave.mk
+
+# Include CM audio files
+include vendor/cm/config/cm_audio.mk
+
+# add Live Wallpapers in device config
+PRODUCT_PACKAGES += \
+    LiveWallpapersPicker \
+    librs_jni
+
+PRODUCT_PACKAGES += \
+    VideoEditor \
+    libvideoeditor_jni \
+    libvideoeditor_core \
+    libvideoeditor_osal \
+    libvideoeditor_videofilters \
+    libvideoeditorplayer
---
+++ vendorsetup.sh
@@ -0,0 +1,4 @@
+if [[ "$YAMATO_APPLY" -eq 1 ]]
+then
+  sh vendor/yamato/source/apply.sh
+fi
